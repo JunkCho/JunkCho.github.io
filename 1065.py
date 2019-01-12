e18f764b5f7bebb1d73968cac803ba6ab6ac387f
Woo Yeong CHO <46465539+JunkCho@users.noreply.github.com>
2019-01-13 00:04:33 +0900

    Create 1065.py

---
+++ 1065.py
@@ -0,0 +1,11 @@
+A = int(input())
+temp =0
+if A <=99:
+    print(A)
+elif A>=1000:
+    print(144)
+else :
+    for i in range(100,A+1): 
+        if (i//100- ((i-100*(i//100))//10))== (i-100*(i//100))//10 - ((i-100*(i//100))-(10*((i-100*(i//100))//10))):
+            temp += 1
+    print(temp+99) 
